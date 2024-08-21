7619b885c330f457aa109dd86ce324745d7adc1e
Set Arteaga <desarrolloecom2@americancotton.com.mx>
2024-08-21 16:34:38 -0600

    Creating the relationship between the providers model and the status model

---
+++ models/Provider.js
@@ -0,0 +1,86 @@
+const { DataTypes, Model} = require('sequelize');
+const sequelize = require('../config/database');
+const State = require('./State');
+
+class Provider extends Model {}
+
+Provider.init({
+    id_provider:{
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+    },
+    name_provider:{
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len:[1, 200]
+        }
+    },
+    description_provider:{
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len:[1, 200]
+        }
+    },
+    country_provider:{
+        type: DataTypes.STRING, 
+        allowNull: false,
+        validate:{
+            len:[1, 200]   
+        }
+    },
+    postcode_provider:{
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate:{
+            len:[1, 10]
+        }
+    },
+    street_provider:{
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate:{
+            len:[1, 200]
+        }
+    },
+    number_provider:{
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate:{
+            len:[1, 10]
+        }
+    },
+    state_provider:{
+        type:DataTypes.STRING,
+        allowNull: false,
+        validate:{
+            len:[1, 200]
+        }
+    },
+    city_provider:{
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate:{
+            len:[1, 200]
+        }
+    },
+    status_provider:{
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references:{
+            model: State,
+            key: 'id_status'
+          }
+    },
+},{
+    sequelize,
+    modelName:'Provider',
+    tableName:'Providers',
+    timestamps: true,
+    underscored: true
+});
+
+module.exports = Provider;
