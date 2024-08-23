78eedee16bf54f5554d0633c24338dafd6d787d1
Set Arteaga <desarrolloecom2@americancotton.com.mx>
2024-08-22 18:15:06 -0600

    WMS-25 [FEATURE] - create endpoint for user creation

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,8 @@ const bcrypt = require('bcryptjs');
 const { User } = require('../models'); // Importa el modelo User de Sequelize
 const ErrorResponse = require('../utils/errorResponse');
 const successHandler = require('../middleware/successHandler/successHandler.middleware');
+const { Op } = require('sequelize');
+
 
 
 exports.getProfile = (req, res, next) => {
@@ -52,7 +54,7 @@ exports.getUserById = async (req, res, next) => {
   const userId = req.params.id;
 
   try {
-    const user = await User.findByPk(userId);
+    const user = await User.findByPk(userId, { attributes: ["id_user", 'username_user', 'email_user', 'role_user', 'isactive_user', 'created_at', 'updated_at']});
     if (!user) {
       return next(new ErrorResponse('User not found', 404));
     }
@@ -65,19 +67,43 @@ exports.getUserById = async (req, res, next) => {
 exports.updateEncryptedPassword = async (req, res, next) => {
   const userId = req.params.id;
   const { newPassword } = req.body;
-
-  try {
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(newPassword, salt);
-
-    const user = await User.findByPk(userId);
-    if (!user) {
-      return next(new ErrorResponse('User not found', 404));
+  if(newPassword){
+    try {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(newPassword, salt);
+  
+      const user = await User.findByPk(userId);
+      if (!user) {
+        return next(new ErrorResponse('User not found', 404));
+      }
+      user.password_user = hashedPassword;
+      await user.save();
+      successHandler(req, res, user.id_user, 'Encrypted password updated successfully');
+    } catch (err) {
+      next(err);
     }
-    user.password = hashedPassword;
-    await user.save();
-    res.status(200).json({ success: true, message: 'Encrypted password updated successfully' });
-  } catch (err) {
-    next(err);
+  }else{
+    return next(new ErrorResponse("newPassword are required fields",401))
   }
+
+  
 };
+
+exports.createUser = async (req, res, next) => {
+  
+  try{
+      const { username, password, email, role } = req.body;
+      console.log('body',req.body)
+      const exisitsUser = await User.findOne({ where: {[Op.or]: [{ username_user: username }, { email_user: email }] } });
+        if(exisitsUser){
+          return next(new ErrorResponse('Username or email already exists', 400));
+        }
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const newUser = await User.create({ username_user: username, password_user: hashedPassword, email_user: email, role_user: role});
+      successHandler(req, res, newUser.id_user, 'User created successfully');
+
+      } catch(err) {
+        next(err);
+    }
+
+}
