33bf4073f572393a812cbecbf735f6e9774726df
Set Arteaga <desarrolloecom2@americancotton.com.mx>
2024-08-26 10:54:05 -0600

    WMS-36 [FEATURE] - Create state by using an endpoint and the routes for each endpoint.

---
+++ routes/statusRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const { protect } = require('../middleware/authMiddleware');
+const { createStatus,updateStatus,deleteStatus,getAllStatus,getStatusById,} = require('../controllers/statusController');
+
+
+router.post('/', protect, createStatus);
+router.put('/:id', protect, updateStatus);
+router.delete('/:id', protect, deleteStatus);
+router.get('/', protect, getAllStatus);
+router.get('/:id', protect,  getStatusById);
+
+
+module.exports = router;
